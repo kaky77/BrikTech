51a65a343e20ca81146afe7fff7c1abf394cd4fc
kader <karambirikader73@gmail.com>
2025-08-16 14:50:15 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BrikTech/precache-manifest.d8e344dde047402c1f1c94230920d6e5.js"
+  "/BrikTech/precache-manifest.4708b206792912da99b7a40c52eebbe3.js"
 );
 
 self.addEventListener('message', (event) => {
